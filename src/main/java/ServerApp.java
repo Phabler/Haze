8dab87a23751b6558e9eb9ffbbf2f85e49d70cfa
Phabler <Phabler@yandex.ru>
2020-07-18 14:15:35 +0300

    init

---
+++ src/main/java/ServerApp.java
@@ -0,0 +1,13 @@
+
+import java.util.logging.Logger;
+
+public class ServerApp {
+
+    private static Logger log = Logger.getLogger(ServerApp.class.getName());
+
+    public static void main(String[] args) {
+        log.info("Hello Server");
+    }
+
+
+}
